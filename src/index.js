981320b918b1f81fe07af2f8d2deae3c428d394d
rniheeth44-eng <rniheeth44@gmail.com>
2026-03-14 17:09:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Web server running on port 3000")
 })
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Bot is running")
+})
+
+app.listen(3000, () => {
+  console.log("Web server running on port 3000")
+})
 const path = require("path");
 
 const {
